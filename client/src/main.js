e28d9595a7c5d43fc5da4574f713fbf28cd3c76c
ngthuongdoan <ngthuongdoan@gmail.com>
2020-07-30 12:30:28 +0700

    rebase non dashboard

--- client/src/main.js
+++ client/src/main.js
@@ -3,13 +3,9 @@ import App from "@/App.vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import router from "./router/index";
-import PaperDashboard from "./components/AdminComponents/plugins/paperDashboard";
-import "vue-notifyjs/themes/default.css";
 
 axios.defaults.baseURL = `https://mrvans.herokuapp.com/api`;
 Vue.use(VueAxios, axios);
-
-Vue.use(PaperDashboard);
 
 Vue.config.productionTip = false;
 export const eventBus = new Vue();
--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,62 +1,7 @@
-import DashboardLayout from "@/components/AdminComponents/layout/dashboard/DashboardLayout.vue";
-// GeneralViews
-import NotFound from "@/components/AdminComponents/pages/NotFoundPage.vue";
-
-// Admin pages
-import Dashboard from "@/components/AdminComponents/pages/Dashboard.vue";
-import UserProfile from "@/components/AdminComponents/pages/UserProfile.vue";
-import Notifications from "@/components/AdminComponents/pages/Notifications.vue";
-import Icons from "@/components/AdminComponents/pages/Icons.vue";
-import Maps from "@/components/AdminComponents/pages/Maps.vue";
-import Typography from "@/components/AdminComponents/pages/Typography.vue";
-import TableList from "@/components/AdminComponents/pages/TableList.vue";
 import Main from "@/components/MainComponents/Main.vue";
 
 const routes = [
   { path: "/", component: Main },
-  {
-    path: "/",
-    component: DashboardLayout,
-    redirect: "/dashboard",
-    children: [
-      {
-        path: "dashboard",
-        name: "dashboard",
-        component: Dashboard
-      },
-      {
-        path: "stats",
-        name: "stats",
-        component: UserProfile
-      },
-      {
-        path: "notifications",
-        name: "notifications",
-        component: Notifications
-      },
-      {
-        path: "icons",
-        name: "icons",
-        component: Icons
-      },
-      {
-        path: "maps",
-        name: "maps",
-        component: Maps
-      },
-      {
-        path: "typography",
-        name: "typography",
-        component: Typography
-      },
-      {
-        path: "table-list",
-        name: "table-list",
-        component: TableList
-      }
-    ]
-  },
-  { path: "*", component: NotFound }
 ];
 
 /**
