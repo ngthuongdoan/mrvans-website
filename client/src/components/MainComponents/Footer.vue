6e0a5feead7b517892deb3cf5ab515b08bb3c6bd
ngthuongdoan <ngthuongdoan@gmail.com>
2020-08-02 11:23:27 +0700

    FB #1

--- client/src/components/MainComponents/Footer.vue
+++ client/src/components/MainComponents/Footer.vue
@@ -11,10 +11,6 @@
   </footer>
 </template>
 
-<script>
-export default {};
-</script>
-
 <style lang="scss">
 footer {
   width: 100%;
--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,13 @@ import App from "@/App.vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import router from "./router/index";
+import VueFbCustomerChat from "vue-fb-customer-chat";
+
+Vue.use(VueFbCustomerChat, {
+  page_id: "109226167490414", //  change 'null' to your Facebook Page ID,
+  theme_color: '#0084ff', // theme color in HEX
+  locale: 'en_US', // default 'en_US'
+})
 
 axios.defaults.baseURL = `https://mrvans.herokuapp.com/api`;
 Vue.use(VueAxios, axios);
