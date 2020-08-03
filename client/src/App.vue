dffe6f94cf3b58593db9dc3fd50ca74f435126c8
ngthuongdoan <ngthuongdoan@gmail.com>
2020-08-03 17:08:04 +0700

    rebuild

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,12 +9,28 @@
       data-width="350"
       data-height="420"
     ></div>
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <div
+      class="fb-customerchat"
+      attribution="setup_tool"
+      page_id="109226167490414"
+      theme_color="#0084ff"
+    ></div>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  mounted() {
+    window.fbAsyncInit = function () {
+      FB.init({
+        xfbml: true,
+        version: "v7.0",
+      });
+    };
+  },
 };
 </script>
 
