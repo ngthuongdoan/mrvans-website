3409a1846dbbc15c9e538b1ae5d7117efdfc22c3
Doan Ngoc Thuong <45509523+ngthuongdoan@users.noreply.github.com>
2020-10-12 09:17:45 +0700

    Change repo

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ Vue.use(VueSweetalert2);
 Vue.use(VTooltip)
 
 // Install BootstrapVue
-// axios.defaults.baseURL = `https://mrvans2020.herokuapp.com/api`;
-axios.defaults.baseURL = `http://192.168.10.105:8081/api`;
+axios.defaults.baseURL = `https://mrvans2020.herokuapp.com/api`;
+// axios.defaults.baseURL = `http://192.168.10.105:8081/api`;
 Vue.config.productionTip = false;
 export const eventBus = new Vue();
 /* eslint-disable no-new */
